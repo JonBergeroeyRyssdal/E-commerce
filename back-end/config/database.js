fcf3da3aca26c86a7f8068b352df6a5519b24a95
Jon Bergerøy Ryssdal <jon@ryssdal.net>
2026-02-27 22:22:17 +0100

    prøver kontakt med aiven

--- back-end/config/database.js
+++ back-end/config/database.js
@@ -2,6 +2,11 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 require('dotenv').config();
 
+console.log('DB_HOST raw:', JSON.stringify(process.env.DB_HOST));
+console.log('DB_PORT raw:', JSON.stringify(process.env.DB_PORT));
+console.log('DB_NAME raw:', JSON.stringify(process.env.DB_NAME));
+console.log('DB_USER raw:', JSON.stringify(process.env.DB_USER));
+
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
